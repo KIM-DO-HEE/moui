0c9d9d9f4bff880b298553dea7a7ddc61e9a98b2
yunbinni <yunbin9049@gmail.com>
2023-01-18 13:12:34 +0900

    Initial commit

---
+++ src/main/java/yunbinni/moui/MouiApplication.java
@@ -0,0 +1,13 @@
+package yunbinni.moui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MouiApplication.class, args);
+	}
+
+}
